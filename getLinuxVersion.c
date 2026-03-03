1bca043461336b5f8dc5c10244d5e6975b1ea958
sY.hf <ybsonmalaquias@gmail.com>
2026-03-02 21:56:37 -0300

    Add files via upload

---
+++ getLinuxVersion.c
@@ -0,0 +1,28 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_MAX_SIZE 1024
+////////////////////////////
+typedef int FILE_DIRECTORY;
+
+int main(void)
+{
+
+ char buffer[BUFFER_MAX_SIZE] = {0};
+ FILE_DIRECTORY file = open("data.txt", O_CREAT | O_WRONLY, 0644);
+ FILE_DIRECTORY file_data = open("/proc/version", O_RDONLY);
+
+ ssize_t bytes_read = read(file_data, buffer, sizeof(buffer));
+ // ssize_t read() -> return bytes read
+ // 0 -> EOF, -1 -> Error
+
+ if (bytes_read > 0) 
+ {
+   write(file, buffer, bytes_read); 
+ }
+
+ close(file);
+ close(file_data); 
+
+ return 0;
+}
